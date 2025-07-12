b408bf1de54a3abc821511d39b03b9ea361f2158
6127613876-1 <gokulpriyan781@gmail.com>
2025-07-12 22:49:12 +0530

    wallet_1

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import { metamaskWallet } from "@thirdweb-dev/react";
 import { Sepolia } from "@thirdweb-dev/chains";
 
 import { StateContextProvider } from './context';
@@ -9,10 +10,13 @@ import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const ActiveChainId = ChainId.Sepolia;
 
 root.render(
-  <ThirdwebProvider activeChain={Sepolia} clientId="8022e6ab63f8d1b23ad3be822eedf887">
+  <ThirdwebProvider
+    activeChain={Sepolia}
+    clientId="8022e6ab63f8d1b23ad3be822eedf887"
+    supportedWallets={[metamaskWallet()]}  // ✅ Add this line
+  >
     <Router>
       <StateContextProvider>
         <App />
